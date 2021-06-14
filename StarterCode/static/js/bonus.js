8b7c5546f35624c65525250edb6302baad4d1f7d
ybgandhi <gandhibyash@gmail.com>
2021-06-14 00:24:49 -0400

    Update bonus.js
    trig to calc meter point (cos , sin)

--- StarterCode/static/js/bonus.js
+++ StarterCode/static/js/bonus.js
@@ -1,4 +1,14 @@
 // test json file
 
-var obj = {message: 'Hello'}
-obj.message
+function Gauge(wfreq) {
+    // enter the washing frequency between 0 and 100
+    var level = parseFloat(wfreq) * 20;
+
+    // trig to calc meter point
+    var degrees = 100 - level;
+    var radius = 0.5l
+    var radians = (degrees * Math.PI) /180;
+    var x = radius * Math.cos(radains);
+    var y = radius * Math.sin(radians);
+
+}
