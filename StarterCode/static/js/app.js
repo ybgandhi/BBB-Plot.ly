bb3ac42d9abbc3b4b157b3d0b101c929cd104e2e
ybgandhi <gandhibyash@gmail.com>
2021-06-13 23:58:36 -0400

    update script

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,35 +1,41 @@
 // Function to pull names from json file and add them in the filter
+var obj = {message: 'Hello'};
+obj.message;
 
-var addChart = function(x_data, y_data, hoverText, metadata) {
+// var addChart = function(x_data, y_data, hoverText, metadata) {
     
-    var metadata_panel = d3.select("#sameples-metadata");
-    metadata_panel.html("");
-    Object.entries(metadata).forEach(([key, value])=>{
-        metadata_panel.append("p").text(`${key}: ${value}`);
-    });
+//     var metadata_panel = d3.select("#sameples-metadata");
+//     metadata_panel.html("");
+//     Object.entries(metadata).forEach(([key, value])=>{
+//         metadata_panel.append("p").text(`${key}: ${value}`);
+//     });
 
-    var trace = {
-        x: x_data,
-        y: y_data,
-        text: hoverText,
-        type: 'bar',
-        orientation: 'h'
-    };
+//     var trace = {
+//         x: x_data,
+//         y: y_data,
+//         text: hoverText,
+//         type: 'bar',
+//         orientation: 'h'
+//     };
     
-    var data = [trace];
+//     var data = [trace];
 
-    Plotly.newPlot('bar', data);
+//     Plotly.newPlot('bar', data);
 
-    var trace2 = {
-        x: x_data,
-        y: y_data,
-        text: hoverText,
-        mode: 'markers',
-        marker: {
-            size: y_data,
-            color: x_data
-        }
-    };
+//     var trace2 = {
+//         x: x_data,
+//         y: y_data,
+//         text: hoverText,
+//         mode: 'markers',
+//         marker: {
+//             size: y_data,
+//             color: x_data
+//         }
+//     };
 
-    
-}
+//     var data2 = [trace2];
+
+//     Plotly.newPlot('bubble', data2);
+
+
+// };
