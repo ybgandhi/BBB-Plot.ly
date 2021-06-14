5bf56796fefb773bcfc2da049f9382e108197018
ybgandhi <gandhibyash@gmail.com>
2021-06-14 00:06:08 -0400

    Update app.js
    d3,json function at the end

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,41 +1,69 @@
 // Function to pull names from json file and add them in the filter
 var obj = {message: 'Hello'};
-obj.message;
+obj.message
 
-// var addChart = function(x_data, y_data, hoverText, metadata) {
+var addChart = function(x_data, y_data, hoverText, metadata) {
     
-//     var metadata_panel = d3.select("#sameples-metadata");
-//     metadata_panel.html("");
-//     Object.entries(metadata).forEach(([key, value])=>{
-//         metadata_panel.append("p").text(`${key}: ${value}`);
-//     });
+    var metadata_panel = d3.select("#sameples-metadata");
+    metadata_panel.html("");
+    Object.entries(metadata).forEach(([key, value])=>{
+        metadata_panel.append("p").text(`${key}: ${value}`);
+    });
 
-//     var trace = {
-//         x: x_data,
-//         y: y_data,
-//         text: hoverText,
-//         type: 'bar',
-//         orientation: 'h'
-//     };
+    var trace = {
+        x: x_data,
+        y: y_data,
+        text: hoverText,
+        type: 'bar',
+        orientation: 'h'
+    };
     
-//     var data = [trace];
+    var data = [trace];
 
-//     Plotly.newPlot('bar', data);
+    Plotly.newPlot('bar', data);
 
-//     var trace2 = {
-//         x: x_data,
-//         y: y_data,
-//         text: hoverText,
-//         mode: 'markers',
-//         marker: {
-//             size: y_data,
-//             color: x_data
-//         }
-//     };
+    var trace2 = {
+        x: x_data,
+        y: y_data,
+        text: hoverText,
+        mode: 'markers',
+        marker: {
+            size: y_data,
+            color: x_data
+        }
+    };
 
-//     var data2 = [trace2];
+    var data2 = [trace2];
 
-//     Plotly.newPlot('bubble', data2);
+    Plotly.newPlot('bubble', data2);
 
 
-// };
+};
+
+var populateDropdown = function(names) {
+    var selecTag = d3.select("#selDataset");
+    var options = selectTag.selectAll('option').data(names);
+
+    options.enter()
+        .append('option')
+        .attr('value',function(d){
+            return d;
+        })
+        .text(function(d) {
+            return d;
+        })
+};
+
+d3.json("static/samples.json").then(function(data) {
+    // populate dropdown with names
+    populateDropdown(data["names"]);
+
+    // populate the page with the first value
+    x_data = data["samples"][0]["otu_ids"];
+    y_data = data["samples"][0]["sample_values"];
+    hoverText = data["samples"][0]["otu_labels"];
+    metadata = data["metadata"][0];
+
+    // add the chart on load
+    addChart(x_data,y_data, hoverText, metadata);
+});
